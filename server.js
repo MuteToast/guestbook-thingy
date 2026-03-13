7436651b8bfe94c5d2ee03c165d0aa889cd53fd1
MuteToast <theoristo7@gmail.com>
2026-03-13 18:30:20 +0100

    1.0.0
    Ok, all the basic done... now the bug fixing... wooo~

--- server.js
+++ server.js
@@ -4,19 +4,41 @@ const expressLayouts = require("express-ejs-layouts");
 
 const PORT = 3000;
 
-//app.use(expressLayouts.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
 app.use(expressLayouts);
 
+let entries = [];
+
 app.get("/", (req, res) => {
-    res.render("index", { title: "Startsida" });
+    res.render("index", { title: "Guestbook" });
 });
 
 app.get("/write", (req, res) => {
     res.render("form", { error: null });
+});
+
+app.post("/write", (req, res) => {
+
+    const name = req.query.name;
+    const message = req.body.message;
+
+    const entry = {
+        name: name,
+        message: message,
+        date: new Date().toLocaleString()
+    };
+
+    entries.push(entry);
+
+    res.redirect("/entries");
+})
+
+app.get("/entries", (req, res) => {
+    res.render("entries", { entries });
 });
 
 app.listen(PORT, () => {
