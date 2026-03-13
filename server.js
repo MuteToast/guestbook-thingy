4d0fb97197756bcf79ce40b20fa8ee689c5c632e
MuteToast <theoristo7@gmail.com>
2026-03-13 17:51:59 +0100

    0.0.4
    Get write now works

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.get("/", (req, res) => {
     res.render("index", { title: "Startsida" });
 });
 
+app.get("/write", (req, res) => {
+    res.render("form", { error: null });
+});
+
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);	
 });
